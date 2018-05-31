9627216ba71ff21c16fe8b5e2e6f1571b971e074
Jesús Sánchez <jesestsana93@hotmail.com>
2018-05-31 14:38:52 -0500

    Add archives

--- dependency_injection/controller.js
+++ dependency_injection/controller.js
@@ -1,9 +1,9 @@
-var app = angular.module("MyFirstApp",[]); 
-app.controller("FirstController", function($scope){
-	$scope.nombre = "Jesus"; 
-	//$scope.nuevoComentario = {comentario:"Hola mundo"};
-	$scope.nuevoComentario = {};
-	$scope.comentarios=[
+angular.module("MyFirstApp",[]) 
+	.controller("FirstController", ["$scope", function(m){
+	m.nombre = "Jesus"; 
+	//m.nuevoComentario = {comentario:"Hola mundo"};
+	m.nuevoComentario = {};
+	m.comentarios=[
 		{
 			comentario:"Buen tutorial",
 			username:"codigofacilito"
@@ -13,8 +13,12 @@ app.controller("FirstController", function($scope){
 			username:"otro_usuario"
 		},
 	];
-	$scope.agregarComentario = function(){
-		$scope.comentarios.push($scope.nuevoComentario);
+	m.agregarComentario = function(){
+		m.comentarios.push(m.nuevoComentario);
+		m.nuevoComentario = {};
 	}
-});
+}]);
+
+//Dependency injection: Las dependencias que uno creo como services o factories o las que ya existen en Angular se 
+//pueden incrustar en los controladores o modulos. Esto se hace a traves del nombre de las variables
 
